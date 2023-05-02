f1762980982ed5a59cd06b3fdef8f9ae1127ea82
zadigg <abelkibebe5@gmail.com>
2023-05-02 17:43:26 -0500

    🔧 chore(compiler.xml): add default annotation processing profile to enable annotation processing 🔧 chore(pom.xml): add testcontainers-bom dependency management to manage testcontainers dependencies 🚀 feat(ProductController.java): add getAllProducts endpoint to retrieve all products 🚀 feat(ProductResponse.java): add ProductResponse DTO to represent product data in responses 🚀 feat(ProductService.java): add getAllProducts method to retrieve all products and map them to ProductResponse DTOs

--- ProductService/src/main/java/com/abelkibebe/productservice/service/ProductService.java
+++ ProductService/src/main/java/com/abelkibebe/productservice/service/ProductService.java
@@ -1,14 +1,18 @@
 package com.abelkibebe.productservice.service;
 
 import com.abelkibebe.productservice.dto.ProductRequest;
+import com.abelkibebe.productservice.dto.ProductResponse;
 import com.abelkibebe.productservice.model.Product;
 import com.abelkibebe.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final ProductRepository productRepository;
@@ -20,5 +24,21 @@ public class ProductService {
                 .build();
 
         productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
     }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product){
+        return ProductResponse.builder()
+                .id(product.getId())
+                .description(product.getDescription())
+                .name(product.getName())
+                .price(product.getPrice())
+                .build();
+    }
+
 }
