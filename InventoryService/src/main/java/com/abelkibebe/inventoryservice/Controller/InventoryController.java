777afceaa50c16dc56af0dd1c1c93ad7b004ee00
zadigg <abelkibebe5@gmail.com>
2023-05-03 15:23:06 -0500

    🔨 refactor(InventoryController.java): inject InventoryService dependency using Lombok's RequiredArgsConstructor annotation ✨ feat(InventoryService.java): add @Transactional annotation to isInStock method to ensure read-only transaction

--- InventoryService/src/main/java/com/abelkibebe/inventoryservice/Controller/InventoryController.java
+++ InventoryService/src/main/java/com/abelkibebe/inventoryservice/Controller/InventoryController.java
@@ -1,15 +1,20 @@
 package com.abelkibebe.inventoryservice.Controller;
 
+import com.abelkibebe.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/inventory")
+@RequiredArgsConstructor
 public class InventoryController {
+
+    private final InventoryService inventoryService;
 
     @GetMapping("/{sku-code}")
     @ResponseStatus(HttpStatus.OK)
     public boolean isInStock(@PathVariable("sku-code") String skuCode) {
-
+        return inventoryService.isInStock(skuCode);
     }
 }
