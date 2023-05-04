384e1c38988456c705243290dbde3012b8153681
zadigg <abelkibebe5@gmail.com>
2023-05-03 19:03:19 -0500

    🔥 chore(.gitignore): remove unnecessary files and directories from version control 🔥 chore(maven-wrapper): remove maven-wrapper files from version control
    🔥 chore(mvnw): remove mvnw file as it is no longer needed and is not being used in the project
    
    🔥 chore(InventoryService): remove unused files and code
    
    🗑️ chore: remove unused files and configurations from the project
    
    🔥 chore(OrderService): remove mvnw file as it is not needed anymore
    
    🔥 chore(OrderService): remove unnecessary files and code
    
    🗑️ chore(OrderService): delete unused files and configurations
    
    🔥 chore(ProductService): remove mvnw file as it is not needed anymore
    
    🗑️ chore(ProductService): remove unused files and code
    
    🗑️ chore(ProductService): remove unused files and code

--- OrderService/src/main/java/com/abelkibebe/orderservice/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.abelkibebe.orderservice.repository;
-
-import com.abelkibebe.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
--- ProductService/src/main/java/com/abelkibebe/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abelkibebe.productservice.repository;
-
-import com.abelkibebe.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
