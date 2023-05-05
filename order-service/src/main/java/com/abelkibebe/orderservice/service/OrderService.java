141914faa6b90a1bcdbcc80bc056d6c6384b937f
zadigg <abelkibebe5@gmail.com>
2023-05-04 21:19:11 -0500

    🐛 fix(uiDesigner.xml): add newline at end of file ✨ feat(discovery-server): add pom.xml file with dependencies for spring-cloud-starter-netflix-eureka-server ✨ feat(InventoryController.java): add endpoint to check inventory for multiple sku codes ✨ feat(InventoryResponse.java): add DTO for inventory response 🐛 fix(InventoryRepository.java): change findBySkuCode to findBySkuCodeIn to support multiple sku codes
    🐛 fix(InventoryService.java): change isInStock method to return a list of InventoryResponse objects instead of a boolean to provide more information
    ✨ feat(application.properties): change hibernate.ddl-auto to create-drop to recreate database schema on every run
    ✨ feat(WebClientConfig.java): add WebClient bean to be used for making HTTP requests
    ✨ feat(OrderService.java): add webClient field to be used for making HTTP requests to inventory service and add logic to check if product is in stock before placing order.

--- order-service/src/main/java/com/abelkibebe/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/abelkibebe/orderservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.abelkibebe.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +19,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -26,6 +28,26 @@ public class OrderService {
                 .map(this::mapToDto)
                 .toList();
         order.setOrderLineItemsList(orderLineItems);
+
+        order.setOrderLineItemsList(orderLineItems);
+        List<String> skuCodes = order.getOrderLineItemsList()
+                .stream().map(OrderLineItems::getSkuCode)
+                .toList();
+
+        //call Inventory service, and place order if product is in stock
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+
+        if (result){
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
+
+
         orderRepository.save(order);
     }
 
